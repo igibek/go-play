07cf17a34ce178382bf698e68468884e515ab43a
igibek <koishybayev.igibek@gmail.com>
2025-10-15 19:41:11 -0400

    go mvc example

---
+++ bookstore/pkg/controllers/book-controller.go
@@ -0,0 +1,23 @@
+package controllers
+
+import "net/http"
+
+func GetBooks(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func GetBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func CreateBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func UpdateBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func DeleteBook(w http.ResponseWriter, r *http.Request) {
+
+}
